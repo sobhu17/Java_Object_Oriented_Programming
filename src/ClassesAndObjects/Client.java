9d3220180e5aaf36c3a152efd5769366f9fd9c7a
saurabh kaushik <habsaurabh1702@gmail.com>
2023-08-04 16:05:29 +0530

    OOP Basics

---
+++ src/ClassesAndObjects/Client.java
@@ -0,0 +1,17 @@
+package ClassesAndObjects;
+
+public class Client {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+//        s.id = 10; can't be initialized as id is a private varibale
+//        and can be accessed only within a class or by getter and setter
+
+        s1.name = "Saurabh Kaushik";
+        s1.batch = "March-22-Intermediate";
+        s1.printStudent();
+
+        Student s2 = new Student();
+        s2.name = "Habeeba Ahmed";
+        s2.printStudent();
+    }
+}
---
+++ src/ClassesAndObjects/Student.java
@@ -0,0 +1,14 @@
+package ClassesAndObjects;
+
+public class Student {
+    private int id;
+    String name;
+    String batch;
+
+    public void printStudent(){
+        System.out.println("id : " + this.id);
+        System.out.println("Name : " + this.name);
+        System.out.println("Batch : " + this.batch);
+    }
+
+}
